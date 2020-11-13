6e1fbd1ee66e7ecfb525426a0ba988072f6c71e1
stevenkcolin <steven.k.colin@gmail.com>
2020-11-14 07:47:40 +0800

    submit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # hardhat-test
 hardhat-test
+
+# launch localhost network
+npx hardhat node
+
+# test
+npx hardhat test
+
+# deploy
+### deploy to testnet
+npx hardhat run scripts/sample-script.js --network ropsten
+
+### deploy to localhost
+npx hardhat run scripts/sample-script.js --network localhost
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-truffle5");
+require("@nomiclabs/hardhat-web3");
+
+const INFURA_PROJECT_ID = process.env.INFURA_PROJECT_ID || "";
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY || "";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,6 +15,15 @@ task("accounts", "Prints the list of accounts", async () => {
     console.log(account.address);
   }
 });
+
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async taskArgs => {
+    const account = web3.utils.toChecksumAddress(taskArgs.account);
+    const balance = await web3.eth.getBalance(account);
+
+    console.log(web3.utils.fromWei(balance, "ether"), "ETH");
+  });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -26,6 +40,13 @@ module.exports = {
       }
     }
   },
+  defaultNetwork: "ropsten",
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  },
   paths: {
     sources: "./contracts",
     tests: "./test",
@@ -33,5 +54,8 @@ module.exports = {
     artifacts: "./artifacts"
   }
 }
+
+
+
 
 
