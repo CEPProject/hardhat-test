a32d68aed158c24fdce67dfb6e1ca51f3701eafe
stevenkcolin <steven.k.colin@gmail.com>
2020-11-13 19:58:34 +0800

    Update sample-test.js

--- test/sample-test.js
+++ test/sample-test.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Greeter", function() {
   it("Should return the new greeting once it's changed", async function() {
@@ -12,3 +13,18 @@ describe("Greeter", function() {
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
+
+describe("Greeter test2", function() {
+  it("Greeter Test2", async function() {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    
+    await greeter.deployed();
+    const [owner, addr1] = await ethers.getSigners();
+
+    // 这里是测试一下用另一个账号来调用signers方法
+    await greeter.connect(addr1).setGreeting("Hallo, Erde!");
+    expect(await greeter.greet()).to.equal("Hallo, Erde!");
+  });
+});
+
